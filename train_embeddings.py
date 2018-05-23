36f86694c814047ce7febb5c2093d6aae2002a0c
Peter Schoener <peter.c.schoener@gmail.com>
2018-05-23 03:14:46 +0200

    bugfixes
    fixed token count minimum (had been set to 1 for debugging)
    fixed missing comma
    fixed file overwrite
    
    I have no idea how this script managed to run during testing

--- train_embeddings.py
+++ train_embeddings.py
@@ -14,15 +14,15 @@ for line in input.read().split("\n") :
 input.close()
 del(input)
 
-model = gensim.models.Word2Vec(sentences, size = 300, window = 10, min_count = 1) # make min count 50 for final test
+model = gensim.models.Word2Vec(sentences, size = 300, window = 10, min_count = 50)
 
 # not really clear that binary True/False makes a difference, but the other module is forced to read as text so that's how we'll do it
-model.wv.save_word2vec_format("./temp-embeddings" binary=False)
+model.wv.save_word2vec_format("./temp-embeddings", binary=False)
 
 # cut off first line because the other module doesn't like it
 temp_file = open("./temp-embeddings")
 model_file = open("./embeddings", "w")
-for line in model_file.read().split("\n")[1:] :
+for line in temp_file.read().split("\n")[1:] :
     model_file.write(line)
     model_file.write("\n")
 model_file.close()
