b59595c3cd3dbb53b2577dbd9e7785228019aca9
Peter Schoener <peter.c.schoener@gmail.com>
2018-06-19 10:33:32 +0200

    updated to simplest working ff network
    still needs to read hyperparams from file
    still needs support for saving trained weights

--- make_graph.py
+++ make_graph.py
@@ -2,36 +2,34 @@
 
 import tensorflow as tf
 
-#class Phase(Enum) :
-#    Train = 0
-#    Validate = 1
-#    Predict = 2
-
-batch_size = 100
+# hyperparameters
+batch_size = 10
 embedding_dim = 300
 hidden_sizes = [1000]
 
-x = tf.placeholder(tf.float32, shape = [batch_size, 2 * embedding_dim], name = "x")
-y = tf.placeholder(tf.float32, name = "y", shape = [batch_size, 1])
+x = tf.placeholder(tf.float32, shape = [2 * embedding_dim, batch_size], name = "x")
+y = tf.placeholder(tf.float32, shape = [1, batch_size], name = "y")
 
+# create and link hidden layers
 hidden = x
 for (i, hidden_size) in enumerate(hidden_sizes) :
-    w = tf.get_variable("w_h_%i" % i, shape = [hidden.shape[1], hidden_size])
-    b = tf.get_variable("b_h_%i" % i, shape = [hidden_size])
+    w = tf.get_variable("w_h_%i" % i, shape = [hidden_size, hidden.shape[0]])
+    b = tf.get_variable("b_h_%i" % i, shape = [hidden_size, 1])
 
-    hidden_out = tf.sigmoid(tf.matmul(hidden, w) + b)
+    hidden = tf.sigmoid(tf.matmul(w, hidden) + b)
 
-    hidden = hidden_out
+# output layer
+w = tf.get_variable("w_o", shape = [y.shape[0], hidden.shape[0]])
+b = tf.get_variable("b_o", shape = [batch_size])
+y_pred = tf.matmul(w, hidden, name = "y_pred") + b
 
-w = tf.get_variable("w_o", shape = [hidden.shape[1], 1])
-b = tf.get_variable("b_o", shape = [1])
-y_pred = tf.matmul(hidden, w) + b
-
-loss = tf.reduce_mean(tf.square(y_pred - y))
+# phases are managed in the main module
+loss = tf.reduce_mean(tf.square(y_pred - y), name = "loss")
 train = tf.train.AdamOptimizer(0.005).minimize(loss, name = "train")
 
 init = tf.variables_initializer(tf.global_variables(), name = "init")
 
+# write graph to file
 definition = tf.Session().graph_def
 tf.train.write_graph(definition, ".", "model.pb", as_text = False)
 
