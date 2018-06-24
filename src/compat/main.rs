dd541fbc455b42ab2191fb9a3999800e3a6b030d
Peter Schoener <peter.c.schoener@gmail.com>
2018-06-24 03:21:15 +0200

    cleaned up dead code and improper style

--- src/compat/main.rs
+++ src/compat/main.rs
@@ -1,6 +1,6 @@
 extern crate rand;
 use rand::distributions::{IndependentSample, Range};
-use rand::{Rng, thread_rng};
+use rand::{thread_rng, Rng};
 
 extern crate rust2vec;
 use rust2vec::{Embeddings, ReadText}; // TODO fix helper script so that this can use ReadWord2Vec instead of ReadText
@@ -12,24 +12,18 @@ use tensorflow::ImportGraphDefOptions;
 use tensorflow::Session;
 use tensorflow::SessionOptions;
 //use tensorflow::Status;
-use tensorflow::StepWithGraph;
-use tensorflow::Tensor;
 use tensorflow::Operation;
 use tensorflow::OutputToken;
+use tensorflow::StepWithGraph;
+use tensorflow::Tensor;
 
-use std::fs;
+//use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::io::{BufRead, BufReader /*, BufWriter*/, Read /*, Write*/};
 
-use std::process::Command;
+//use std::process::Command;
 
 use std::ops::Deref;
-
-/*extern crate itertools;
-use itertools::Itertools;*/
-
-extern crate simd;
-use simd::f32x4;
 
 struct NAdjPair {
     noun: String,
@@ -49,138 +43,7 @@ fn main() {
     let mut pairs: Vec<NAdjPair> = Vec::new();
     let mut rng = rand::thread_rng();
 
-    let root_entry: CoNLLEntry = CoNLLEntry {
-        lemma: "[root]".to_string(),
-        pos: "[root]".to_string(),
-        head: 0u8,
-    };
-
-    /*
-    // scope the file handles to close them when no longer needed
-    {
-        // open pair file
-        let pair_file: File = File::create("./pairs").expect("Error creating pair file");
-        let mut pair_file: BufWriter<_> = BufWriter::new(pair_file);
-
-        // open block text file
-        let text_file: File = File::create("./text").expect("Error creating block text file");
-        let mut text_file: BufWriter<_> = BufWriter::new(text_file);
-
-        // process corpus files one at a time
-        let mut path_idx: usize = 0;
-        let path_ct: usize;
-        for path_container in {
-            let file_list: Vec<std::fs::DirEntry> = fs::read_dir("./corpus/")
-                .unwrap()
-                .map(|r| r.unwrap())
-                .collect();
-            path_ct = file_list.len();
-            file_list
-        } {
-            let path: String = path_container.path().display().to_string();
-            if (&path).ends_with(".conll.gz") {
-                // unzip file
-                Command::new("gzip")
-                    .arg("-df")
-                    .arg(&path)
-                    .status()
-                    .expect("Error unzipping input file");
-
-                // open input file
-                let conll_name: String =
-                    (&path).chars().take((&path).len() - 3).collect::<String>();
-                let input: File = File::open(conll_name.clone()).expect("Error opening input file");
-                let mut sentence: Vec<CoNLLEntry> = Vec::new();
-                sentence.push(root_entry.clone()); // represents head, also makes indices easier
-
-                // read one sentence at a time
-                let mut input: BufReader<_> = BufReader::new(input);
-                for line in input.lines().map(|l| match l {
-                    Ok(s) => s,
-                    Err(..) => String::new(),
-                }) {
-                    // when a sentence boundary (empty line) is hit
-                    if line.len() == 0 {
-                        // find all adjective/noun pairs
-                        for entry in sentence.clone() {
-                            // for each adjective
-                            if entry.pos.starts_with("ADJ") {
-                                let pair: NAdjPair = NAdjPair {
-                                    noun: sentence[entry.head as usize].clone().lemma,
-                                    adj: entry.lemma,
-                                    confidence: 1.0f32, // dummy; will be overwritten
-                                    embedding: vec![0.0f32; 600], // will also be overwritten
-                                };
-
-                                // write pair to file for MI analysis
-                                let pair_string: String =
-                                    format!("{}\t{}\n", &(pair.noun), &(pair.adj));
-                                pair_file
-                                    .write_all(pair_string.as_bytes())
-                                    .expect("Error writing adjective/noun pair to file");
-                            }
-                        }
-
-                        // write sentence to embedding training file
-                        let mut sentence_string: String = sentence
-                            .iter()
-                            .map(|e| {
-                                let mut str: String = e.lemma.clone();
-                                str.push_str(" ");
-                                str
-                            })
-                            .collect();
-                        sentence_string.push_str("\n");
-                        text_file
-                            .write_all(sentence_string.as_bytes())
-                            .expect("Error writing sentence to file");
-
-                        // clear sentence (except root)
-                        sentence.resize(1, root_entry.clone()); // should never need to actually fill anything
-                    } else {
-                        // otherwise, extract the important CoNLL fields
-                        let fields: Vec<String> = line.split_whitespace()
-                            .map(|s| s.to_string())
-                            .collect::<Vec<_>>();
-                        if fields.len() >= 7 {
-                            let entry: CoNLLEntry = CoNLLEntry {
-                                lemma: fields[2].clone(),
-                                pos: fields[3].clone(), // DEBUG: in actual corpus this is 4
-                                head: match fields[6].parse::<u8>() {
-                                    Ok(n) => n,
-                                    Err(..) => 0,
-                                },
-                            };
-                            sentence.push(entry);
-                        } else {
-                            // DEBUG
-                            println!("found short line:");
-                            for field in fields {
-                                println!("\t{}", field);
-                            }
-                        }
-                    }
-                }
-                // rezip file
-                Command::new("gzip")
-                    .arg("-f")
-                    .arg(&conll_name)
-                    .spawn()
-                    .expect("Error rezipping input file");
-            }
-
-            path_idx += 1;
-            print!("{} of {} files processed\r", path_idx, path_ct);
-        }
-        println!("");
-    } // this closes the file handles
-    */
-
-    // train embeddings
-    /*print!("Training embeddings\r");
-    Command::new("./train_embeddings.py")
-        .output()
-        .expect("Could not train embeddings");*/
+    // read embeddings
     print!("Reading embeddings\r");
     let mut embedding_model: Embeddings;
     {
@@ -192,20 +55,11 @@ fn main() {
 
         embedding_model.normalize();
     }
-    //println!("Trained embeddings ");
     println!("Read embeddings   ");
 
-    // run MI utility and capture output
+    // read MI values
     print!("Computing mutual information\r");
-    /*Command::new("compute-mi")
-        .arg("-m")
-        .arg("nsc")
-        .arg("1,2")
-        .arg("./pairs")
-        .arg("./nmi")
-        .status()
-        .expect("Could not compute mutual information"); // do not capture output directly, use buffer since it will be massive
-    */{
+    {
         let pair_file: File = File::open("./nmi").expect("Could not open mutual information file");
         let pair_file: BufReader<_> = BufReader::new(pair_file);
         for line in pair_file.lines().map(|l| match l {
@@ -325,28 +179,32 @@ fn main() {
     // train on 3/4 of data, validate on 1/4
     for batch in 0..batch_ct {
         // concatenate embeddings to get feature vector
-	let x_batch: Tensor<f32>;
-	let y_batch: Tensor<f32>;
-	{
-		let mut vec: Vec<f32>;
-		x_batch = Tensor::new(&[2u64 * (embedding_model.embed_len() as u64), batch_size as u64])
-		    .with_values({
-			vec =
-			    Vec::with_capacity(2 * embedding_model.embed_len() * batch_size);
-			for e in pairs[batch * batch_size..(batch + 1) * batch_size].iter() {
-			    vec.append(&mut e.embedding.clone());
-			}
-			vec.as_mut_slice()
-		    }).unwrap();
-		// concatenate confidences to get output vector
-		y_batch = Tensor::new(&[1u64, batch_size as u64]).with_values({
-		    vec = Vec::with_capacity(batch_size);
-		    for e in pairs[batch * batch_size..(batch + 1) * batch_size].iter() {
-			vec.push(e.confidence);
-		    }
-		    vec.as_mut_slice()
-		}).unwrap();
-	}
+        let x_batch: Tensor<f32>;
+        let y_batch: Tensor<f32>;
+        {
+            let mut vec: Vec<f32>;
+            x_batch = Tensor::new(&[
+                2u64 * (embedding_model.embed_len() as u64),
+                batch_size as u64,
+            ]).with_values({
+                vec = Vec::with_capacity(2 * embedding_model.embed_len() * batch_size);
+                for e in pairs[batch * batch_size..(batch + 1) * batch_size].iter() {
+                    vec.append(&mut e.embedding.clone());
+                }
+                vec.as_mut_slice()
+            })
+                .unwrap();
+            // concatenate confidences to get output vector
+            y_batch = Tensor::new(&[1u64, batch_size as u64])
+                .with_values({
+                    vec = Vec::with_capacity(batch_size);
+                    for e in pairs[batch * batch_size..(batch + 1) * batch_size].iter() {
+                        vec.push(e.confidence);
+                    }
+                    vec.as_mut_slice()
+                })
+                .unwrap();
+        }
 
         // train step
         if batch % 4 != 0 {
@@ -354,8 +212,10 @@ fn main() {
             train_step.add_input(&x, 0, &x_batch);
             train_step.add_input(&y, 0, &y_batch);
             train_step.add_target(&train);
-            
-            session.run(&mut train_step).expect("Could not run training step");
+
+            session
+                .run(&mut train_step)
+                .expect("Could not run training step");
         }
         // validation step
         else {
@@ -363,8 +223,10 @@ fn main() {
             output_step.add_input(&x, 0, &x_batch);
             output_step.add_input(&y, 0, &y_batch);
             let loss_idx: OutputToken = output_step.request_output(&loss, 0);
-            
-            session.run(&mut output_step).expect("Could not run validation step");
+
+            session
+                .run(&mut output_step)
+                .expect("Could not run validation step");
 
             let loss_val: Tensor<f32> = output_step.take_output(loss_idx).unwrap();
             println!("Epoch: {}\t Loss: {}", batch / 4, loss_val.deref()[0]);
@@ -433,34 +295,4 @@ fn main() {
         assert!(!weight.is_nan());
         print!("{} ", weight);
     }
-    println!("]");*/
-}
-
-fn dot(mut u: &[f32], mut v: &[f32]) -> f32 {
-    assert_eq!(u.len(), v.len());
-
-    let mut sums = f32x4::splat(0.0);
-
-    while u.len() >= 4 {
-        let a = f32x4::load(u, 0);
-        let b = f32x4::load(v, 0);
-
-        sums = sums + a * b;
-
-        u = &u[4..];
-        v = &v[4..];
-    }
-
-    sums.extract(0) + sums.extract(1) + sums.extract(2) + sums.extract(3) + dot_slow(u, v)
-}
-
-fn dot_slow(u: &[f32], v: &[f32]) -> f32 {
-    assert_eq!(u.len(), v.len());
-
-    let mut sum: f32 = 0.0f32;
-    for i in 0..u.len() {
-        sum += u[i] * v[i];
-    }
-
-    sum
-}
+    println!("]");*/}
--- src/prep/main.rs
+++ src/prep/main.rs
@@ -1,8 +1,13 @@
 use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
 use std::process::Command;
+
+struct NAdjPair {
+    noun: String,
+    adj: String,
+}
 
 #[derive(Clone)]
 struct CoNLLEntry {
@@ -11,121 +16,124 @@ struct CoNLLEntry {
     head: u8,
 }
 
-fn main()
-{
-   
-        // open pair file
-        let pair_file: File = File::create("./pairs").expect("Error creating pair file");
-        let mut pair_file: BufWriter<_> = BufWriter::new(pair_file);
+fn main() {
+    let root_entry: CoNLLEntry = CoNLLEntry {
+        lemma: "[root]".to_string(),
+        pos: "[root]".to_string(),
+        head: 0u8,
+    };
 
-        // open block text file
-        let text_file: File = File::create("./text").expect("Error creating block text file");
-        let mut text_file: BufWriter<_> = BufWriter::new(text_file);
-        // process corpus files one at a time
-        let mut path_idx: usize = 0;
-        let path_ct: usize;
-        for path_container in {
-            let file_list: Vec<std::fs::DirEntry> = fs::read_dir("./corpus/")
-                .unwrap()
-                .map(|r| r.unwrap())
-                .collect();
-            path_ct = file_list.len();
-            file_list
-        } {
-            let path: String = path_container.path().display().to_string();
-            if (&path).ends_with(".conll.gz") {
-                // unzip file
-                Command::new("gzip")
-                    .arg("-df")
-                    .arg(&path)
-                    .status()
-                    .expect("Error unzipping input file");
+    // open pair file
+    let pair_file: File = File::create("./pairs").expect("Error creating pair file");
+    let mut pair_file: BufWriter<_> = BufWriter::new(pair_file);
 
-                // open input file
-                let conll_name: String =
-                    (&path).chars().take((&path).len() - 3).collect::<String>();
-                let input: File = File::open(conll_name.clone()).expect("Error opening input file");
-                let mut sentence: Vec<CoNLLEntry> = Vec::new();
-                sentence.push(root_entry.clone()); // represents head, also makes indices easier
+    // open block text file
+    let text_file: File = File::create("./text").expect("Error creating block text file");
+    let mut text_file: BufWriter<_> = BufWriter::new(text_file);
+    // process corpus files one at a time
+    let mut path_idx: usize = 0;
+    let path_ct: usize;
+    for path_container in {
+        let file_list: Vec<std::fs::DirEntry> = fs::read_dir("./corpus/")
+            .unwrap()
+            .map(|r| r.unwrap())
+            .collect();
+        path_ct = file_list.len();
+        file_list
+    } {
+        let path: String = path_container.path().display().to_string();
+        if (&path).ends_with(".conll.gz") {
+            // unzip file
+            Command::new("gzip")
+                .arg("-df")
+                .arg(&path)
+                .status()
+                .expect("Error unzipping input file");
 
-                // read one sentence at a time
-                let mut input: BufReader<_> = BufReader::new(input);
-                for line in input.lines().map(|l| match l {
-                    Ok(s) => s,
-                    Err(..) => String::new(),
-                }) {
-                    // when a sentence boundary (empty line) is hit
-                    if line.len() == 0 {
-                        // find all adjective/noun pairs
-                        for entry in sentence.clone() {
-                            // for each adjective
-                            if entry.pos.starts_with("ADJ") {
-                                let pair: NAdjPair = NAdjPair {
-                                    noun: sentence[entry.head as usize].clone().lemma,
-                                    adj: entry.lemma,
-                                    confidence: 1.0f32, // dummy; will be overwritten
-                                    embedding: vec![0.0f32; 600], // will also be overwritten
-                                };
+            // open input file
+            let conll_name: String = (&path).chars().take((&path).len() - 3).collect::<String>();
+            let input: File = File::open(conll_name.clone()).expect("Error opening input file");
+            let mut sentence: Vec<CoNLLEntry> = Vec::new();
+            sentence.push(root_entry.clone()); // represents head, also makes indices easier
 
-                                // write pair to file for MI analysis
-                                let pair_string: String =
-                                    format!("{}\t{}\n", &(pair.noun), &(pair.adj));
-                                pair_file
-                                    .write_all(pair_string.as_bytes())
-                                    .expect("Error writing adjective/noun pair to file");
-                            }
-                        }
-
-                        // write sentence to embedding training file
-                        let mut sentence_string: String = sentence
-                            .iter()
-                            .map(|e| {
-                                let mut str: String = e.lemma.clone();
-                                str.push_str(" ");
-                                str
-                            })
-                            .collect();
-                        sentence_string.push_str("\n");
-                        text_file
-                            .write_all(sentence_string.as_bytes())
-                            .expect("Error writing sentence to file");
-
-                        // clear sentence (except root)
-                        sentence.resize(1, root_entry.clone()); // should never need to actually fill anything
-                    } else {
-                        // otherwise, extract the important CoNLL fields
-                        let fields: Vec<String> = line.split_whitespace()
-                            .map(|s| s.to_string())
-                            .collect::<Vec<_>>();
-                        if fields.len() >= 7 {
-                            let entry: CoNLLEntry = CoNLLEntry {
-                                lemma: fields[2].clone(),
-                                pos: fields[3].clone(), // DEBUG: in actual corpus this is 4
-                                head: match fields[6].parse::<u8>() {
-                                    Ok(n) => n,
-                                    Err(..) => 0,
-                                },
+            // read one sentence at a time
+            let mut input: BufReader<_> = BufReader::new(input);
+            for line in input.lines().map(|l| match l {
+                Ok(s) => s,
+                Err(..) => String::new(),
+            }) {
+                // when a sentence boundary (empty line) is hit
+                if line.len() == 0 {
+                    // find all adjective/noun pairs
+                    for entry in sentence.clone() {
+                        // for each adjective
+                        if entry.pos.starts_with("ADJ") {
+                            let pair: NAdjPair = NAdjPair {
+                                noun: sentence[entry.head as usize].clone().lemma,
+                                adj: entry.lemma,
+                                confidence: 1.0f32, // dummy; will be overwritten
+                                embedding: vec![0.0f32; 600], // will also be overwritten
                             };
-                            sentence.push(entry);
-                        } else {
-                            // DEBUG
-                            println!("found short line:");
-                            for field in fields {
-                                println!("\t{}", field);
-                            }
+
+                            // write pair to file for MI analysis
+                            let pair_string: String =
+                                format!("{}\t{}\n", &(pair.noun), &(pair.adj));
+                            pair_file
+                                .write_all(pair_string.as_bytes())
+                                .expect("Error writing adjective/noun pair to file");
+                        }
+                    }
+
+                    // write sentence to embedding training file
+                    let mut sentence_string: String = sentence
+                        .iter()
+                        .map(|e| {
+                            let mut str: String = e.lemma.clone();
+                            str.push_str(" ");
+                            str
+                        })
+                        .collect();
+                    sentence_string.push_str("\n");
+                    text_file
+                        .write_all(sentence_string.as_bytes())
+                        .expect("Error writing sentence to file");
+
+                    // clear sentence (except root)
+                    sentence.resize(1, root_entry.clone()); // should never need to actually fill anything
+                } else {
+                    // otherwise, extract the important CoNLL fields
+                    let fields: Vec<String> = line.split_whitespace()
+                        .map(|s| s.to_string())
+                        .collect::<Vec<_>>();
+                    if fields.len() >= 7 {
+                        let entry: CoNLLEntry = CoNLLEntry {
+                            lemma: fields[2].clone(),
+                            pos: fields[3].clone(), // DEBUG: in actual corpus this is 4
+                            head: match fields[6].parse::<u8>() {
+                                Ok(n) => n,
+                                Err(..) => 0,
+                            },
+                        };
+                        sentence.push(entry);
+                    } else {
+                        // DEBUG
+                        println!("found short line:");
+                        for field in fields {
+                            println!("\t{}", field);
                         }
                     }
                 }
-                // rezip file
-                Command::new("gzip")
-                    .arg("-f")
-                    .arg(&conll_name)
-                    .spawn()
-                    .expect("Error rezipping input file");
             }
-
-            path_idx += 1;
-            print!("{} of {} files processed\r", path_idx, path_ct);
+            // rezip file
+            Command::new("gzip")
+                .arg("-f")
+                .arg(&conll_name)
+                .spawn()
+                .expect("Error rezipping input file");
         }
-        println!("");
+
+        path_idx += 1;
+        print!("{} of {} files processed\r", path_idx, path_ct);
+    }
+    println!("");
 }
