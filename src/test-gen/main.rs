e7508213826fa04afaa0752b8c580340580ccb78
Peter Schoener <peter.c.schoener@gmail.com>
2018-08-28 16:55:54 +0200

    read from actual sample files
    vastly improves quality of suggested pairs

--- src/test-gen/main.rs
+++ src/test-gen/main.rs
@@ -1,10 +1,7 @@
-use std::collections::HashSet;
-use std::collections::VecDeque;
-
+use std::collections::{HashSet, VecDeque};
 use std::fs::{File, OpenOptions};
-
-use std::io::{BufRead, BufReader, BufWriter, Lines, Write};
-
+use std::io::{BufRead, BufReader, Read, BufWriter, Lines, Write};
+use std::process::{Child, Command, Stdio};
 use std::iter::FromIterator;
 
 extern crate rand;
@@ -17,15 +14,51 @@ fn main() -> () {
     // get all nouns and adjectives
     let mut nouns: HashSet<String> = HashSet::new();
     let mut adjs: HashSet<String> = HashSet::new();
+    // and pairs
+    let mut pairs: Vec<(String, String)> = Vec::new();
     {
-        let input: BufReader<_> =
-            BufReader::new(File::open("pos").expect("Failed to open input file"));
+        let mut input: BufReader<File> =
+            BufReader::new(File::open("pos").expect("Failed to open positive input file"));
         let mut lines: Lines<_> = input.lines();
         while let Some(Ok(line)) = lines.next() {
             let fields: Vec<&str> = Vec::from_iter(line.split_whitespace());
             assert!(fields.len() == 2, "Error splitting line: {}", line);
             nouns.insert(fields[0].to_string());
             adjs.insert(fields[1].to_string());
+        }
+        // shuffle negative and positive samples
+        let mut cat: Child = Command::new("cat")
+            .args(&["pos", "neg", "misclassified"])
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Could not concatenate negative sample files");
+        let mut shuf: Child = Command::new("shuf")
+            .arg("-o")
+            .arg("./all_shuffled")
+            .stdin(Stdio::piped())
+            .spawn()
+            .expect("Could not shuffle negative samples");
+        if let Some(ref mut cat_out) = cat.stdout {
+            if let Some(ref mut shuf_in) = shuf.stdin {
+                // write the whole thing in one go since it shouldn't be more than we can fit in memory
+                let mut buf: Vec<u8> = Vec::with_capacity(10000);
+                cat_out
+                    .read_to_end(&mut buf)
+                    .expect("Could not read end of negative cat output");
+                shuf_in
+                    .write_all(&mut buf)
+                    .expect("Could not write end of negative shuf input");
+            }
+        }
+        shuf.wait()
+            .expect("Could not finish shuffling negative samples");
+        input =
+            BufReader::new(File::open("all_shuffled").expect("Failed to open shuffled input file"));
+        lines = input.lines();
+        while let Some(Ok(line)) = lines.next() {
+            let fields: Vec<&str> = Vec::from_iter(line.split_whitespace());
+            assert!(fields.len() >= 2, "Error splitting line: {}", line);
+            pairs.push((fields[0].to_string(), fields[1].to_string()));
         }
     }
     let mut noun_vec: Vec<String> = Vec::from_iter(nouns.iter().map(|e| e.clone()));
@@ -35,6 +68,7 @@ fn main() -> () {
     let mut rng = rand::thread_rng();
     let mut n_range: Range<usize> = Range::new(0usize, noun_vec.len());
     let mut a_range: Range<usize> = Range::new(0usize, adj_vec.len());
+    let file_range: Range<u8> = Range::new(0u8, 2u8);
 
     // open files for writing
     let mut pos_gs: BufWriter<_> = BufWriter::new(
@@ -57,10 +91,25 @@ fn main() -> () {
     let mut adj: String;
     let undo_buf_len = 10;
     let mut queue: VecDeque<(String, bool)> = VecDeque::with_capacity(undo_buf_len + 1);
+    let mut file_iter = pairs.iter();
     loop {
         // generate pair
-        noun = noun_vec[n_range.ind_sample(&mut rng)].clone();
-        adj = adj_vec[a_range.ind_sample(&mut rng)].clone();
+        match file_range.ind_sample(&mut rng) {
+            0 => {
+                noun = noun_vec[n_range.ind_sample(&mut rng)].clone();
+                adj = adj_vec[a_range.ind_sample(&mut rng)].clone();
+            }
+            _ => match file_iter.next() { // this will only ever be 1, but the compiler doesn't know and will throw an error if the default case isn't covered
+                Some(t) => {
+                    noun = t.0.clone();
+                    adj = t.1.clone();
+                }
+                None => { // TODO make this fall back more elegantly
+                    noun = noun_vec[n_range.ind_sample(&mut rng)].clone();
+                    adj = adj_vec[a_range.ind_sample(&mut rng)].clone();
+                }
+            },
+        }
         print!("{} {}\n", adj, noun);
 
         while match Term::stdout().read_key() {
